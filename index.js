faad4cd70bf27880d839d3f75a15956fc47ad291
Daniel Gu <endaytrer@gmail.com>
2024-05-28 21:57:32 +0800

    mobile animation

--- index.js
+++ index.js
@@ -2,24 +2,45 @@ const bgContainer = document.getElementById("bg-container");
 const balls = document.getElementsByClassName("bg-ball");
 const perspectiveFactor = 0.5;
 
-const pos = [[22, 61], [52, 100], [75, 32]];
+const pos = [[19, 61], [52, 95], [73, 22]];
 const scales = [0.70, 0.55, 0.9];
 const T = [8500, 14600, 12000]
 const A = [15, 30, 20]
 const phi = [0, 3, 1.5]
 
-for (let i = 0; i < 3; i++) {
-    balls[i].style.left = `${pos[i][0]}vw`;
-    balls[i].style.top = `${pos[i][1]}vh`;
-}
+
 function animation(time) {
-    bgContainer.style.transform = `translateY(-${perspectiveFactor * window.scrollY}px)`;
     for (let i = 0; i < 3; i++) {
         const x = A[i] * Math.cos(2 * Math.PI * time / T[i] + phi[i]);
         const y = A[i] * Math.sin(2 * Math.PI * time / T[i] + phi[i]);
-        balls[i].style.transform = `translateX(calc(${x}px - 50%)) translateY(calc(${y}px - 50%)) scale(${scales[i]})`
+        balls[i].style.transform = `translateX(${x}px) translateY(${y - perspectiveFactor * window.scrollY}px)`
     }
     requestAnimationFrame(animation);
 }
 
-requestAnimationFrame(animation);
+window.onload = () => {
+    console.log("Hello!")
+    for (let i = 0; i < 3; i++) {
+        balls[i].style.left = `${pos[i][0]}vw`;
+        balls[i].style.top = `${pos[i][1]}vh`;
+        balls[i].style.width = `${scales[i] * 300}px`;
+        balls[i].style.height = `${scales[i] * 300}px`;
+        balls[i].style.marginTop = `-${scales[i] * 150}px`;
+        balls[i].style.marginLeft = `-${scales[i] * 150}px`;
+    }
+    requestAnimationFrame(animation);
+}
+
+// window.onscroll = () => {
+//     bgContainer.style.transform = `translateY(-${perspectiveFactor * window.scrollY}px)`;
+// }
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
